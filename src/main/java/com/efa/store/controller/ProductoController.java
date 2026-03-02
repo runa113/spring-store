85f8d746d0dd8276118b11ee5b61fea8fff06388
Edgar Facundo Acuña <edgarfacundo3000@gmail.com>
2026-03-01 19:13:43 -0600

    Se agrega documentación de swagger y se ajusta pom

--- src/main/java/com/efa/store/controller/ProductoController.java
+++ src/main/java/com/efa/store/controller/ProductoController.java
@@ -3,13 +3,23 @@ package com.efa.store.controller;
 
 import com.efa.store.entity.Producto;
 import com.efa.store.service.imp.ProductoServiceImp;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.Optional;
 
 @RestController
+@Tag(name = "productos", description = "Servicios asociados a los productos")
 public class ProductoController {
 
     @Autowired
@@ -19,6 +29,13 @@ public class ProductoController {
     public String nombreSitio() {
         return "Tutorial Angular";
     }
+
+    @Operation(summary = "Solicitud que obtiene la configuración de los permisos de los componentes de la ventanilla")
+    @ApiResponse(responseCode = "200", description = "Operación exitosa")
+    //@ApiResponse(description = "Operación exitosa", responseCode = "200", content = {@Content(mediaType = Constantes.MEDIA_TYPE_JSON)}),
+    @ApiResponse(responseCode = "400", description = "Solicitud mal formada o parámetros inválidos.", content = @Content)
+    @ApiResponse(responseCode = "404", description = "El recurso solicitado no se ha encontrado. Verifique los parámetros e intente nuevamente.", content = @Content)
+    @ApiResponse(responseCode = "500", description = "Error interno del servidor al procesar la solicitud.", content = @Content)
 
     @GetMapping("/all")
     public ArrayList<Producto> getAllProductos(){
